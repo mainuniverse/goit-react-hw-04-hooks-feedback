040be1b7a8239a0b79a9b7db61b8069b05888836
Maria <54138760+mainuniverse@users.noreply.github.com>
2022-01-20 17:12:47 +0200

    updt with husky

---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,12 @@
+import s from './FeedbackOptions.module.css';
+
+
+export default function Button({ buttonName, increment }) {
+  return (
+    <li className={s.list}>
+      <button className={s.button} onClick={() => increment(buttonName)}>
+        {buttonName}
+      </button>
+    </li>
+  );
+}
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,26 @@
+import s from './Statistics.module.css';
+//import Notification from './Notification';
+import PropsType from 'prop-types';
+
+
+export default function Statistics({ good, neutral, bad, total, percantage }) {
+  return (
+    <ul className={s.list}>
+      <li>
+        <p className={s.item}>Good: {good}</p>
+      </li>
+      <li>
+        <p className={s.item}>Neutral: {neutral}</p>
+      </li>
+      <li>
+        <p className={s.item}>Bad: {bad}</p>
+      </li>
+      <li>
+        <p className={s.item}>Total: {total} </p>
+      </li>
+      <li>
+        <p className={s.item}>Positive feedback: {percantage}%</p>
+      </li>
+    </ul>
+  );
+}
